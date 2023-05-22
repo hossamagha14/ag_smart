7ea0284783985a769dd8ca6d177e1250f8a8b33f
Your Name <you@example.com>
2023-05-23 00:59:56 +0300

    features list

--- lib/View/Screens/device_features.dart
+++ lib/View/Screens/device_features.dart
@@ -31,128 +31,145 @@ class DeviceFeaturesScreen extends StatelessWidget {
                     child: CircularProgressIndicator(
                     color: Colors.blue,
                   ))
-                : SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        MainCard2(
-                          buttonColor: greenButtonColor,
-                          cardtitle: text[chosenLanguage]!['Device Features']!,
-                          mainWidget: Padding(
-                              padding: const EdgeInsets.only(bottom: 10),
-                              child: SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.8,
-                                height:
-                                    MediaQuery.of(context).size.height * 0.43,
-                                child: ListView.separated(
-                                    physics: const BouncingScrollPhysics(),
-                                    itemBuilder: (context, index) {
-                                      return AnimatedContainer(
-                                        duration:
-                                            const Duration(milliseconds: 0),
-                                        width:
-                                            MediaQuery.of(context).size.width *
+                : Column(
+                    children: [
+                      MainCard2(
+                        buttonColor: greenButtonColor,
+                        cardtitle: text[chosenLanguage]!['Device Features']!,
+                        mainWidget: Padding(
+                            padding: const EdgeInsets.only(bottom: 10),
+                            child: SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.8,
+                              height: MediaQuery.of(context).size.height * 0.43,
+                              child: ListView.separated(
+                                  physics: const BouncingScrollPhysics(),
+                                  itemBuilder: (context, index) {
+                                    return myCubit.featuresIntList[index] == 1
+                                        ? const SizedBox(
+                                            width: 0,
+                                            height: 0,
+                                          )
+                                        : AnimatedContainer(
+                                            duration:
+                                                const Duration(milliseconds: 0),
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
                                                 0.8,
-                                        height: MediaQuery.of(context)
-                                                .size
-                                                .height *
-                                            myCubit.featureList[index].height!,
-                                        decoration: BoxDecoration(
-                                            color: backgroundColor,
-                                            borderRadius:
-                                                BorderRadius.circular(10)),
-                                        child: Padding(
-                                          padding: const EdgeInsets.fromLTRB(
-                                              10, 10, 10, 0),
-                                          child: Column(
-                                            children: [
-                                              Row(
-                                                textDirection:
-                                                    TextDirection.rtl,
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment
-                                                        .spaceBetween,
+                                            height: MediaQuery.of(context)
+                                                    .size
+                                                    .height *
+                                                myCubit
+                                                    .featureList[index].height!,
+                                            decoration: BoxDecoration(
+                                                color: backgroundColor,
+                                                borderRadius:
+                                                    BorderRadius.circular(10)),
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.fromLTRB(
+                                                      10, 10, 10, 0),
+                                              child: Column(
                                                 children: [
                                                   Row(
                                                     textDirection:
                                                         TextDirection.rtl,
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment
+                                                            .spaceBetween,
                                                     children: [
-                                                      const CircleAvatar(
-                                                        backgroundColor:
-                                                            Colors.green,
-                                                        radius: 8,
+                                                      Row(
+                                                        textDirection:
+                                                            TextDirection.rtl,
+                                                        children: [
+                                                          const CircleAvatar(
+                                                            backgroundColor:
+                                                                Colors.green,
+                                                            radius: 8,
+                                                          ),
+                                                          const SizedBox(
+                                                            width: 15,
+                                                          ),
+                                                          Text(myCubit
+                                                              .featureList[
+                                                                  index]
+                                                              .title!),
+                                                        ],
                                                       ),
-                                                      const SizedBox(
-                                                        width: 15,
-                                                      ),
-                                                      Text(myCubit
-                                                          .featureList[index]
-                                                          .title!),
+                                                      InkWell(
+                                                          onTap: () {
+                                                            myCubit
+                                                                .getInfo(index);
+                                                          },
+                                                          child: Text(
+                                                            'p',
+                                                            style: TextStyle(
+                                                                fontFamily:
+                                                                    'icons',
+                                                                color:
+                                                                    iconColor,
+                                                                fontSize: 18),
+                                                          )),
                                                     ],
                                                   ),
-                                                  InkWell(
-                                                      onTap: () {
-                                                        myCubit.getInfo(index);
-                                                      },
+                                                  Padding(
+                                                    padding: const EdgeInsets
+                                                        .fromLTRB(0, 5, 31, 0),
+                                                    child:
+                                                        AnimatedDefaultTextStyle(
+                                                      textAlign:
+                                                          TextAlign.right,
+                                                      style: TextStyle(
+                                                          color: Colors.black,
+                                                          fontSize: myCubit
+                                                              .featureList[
+                                                                  index]
+                                                              .fontSize),
+                                                      duration: const Duration(
+                                                          milliseconds: 0),
                                                       child: Text(
-                                                        'p',
-                                                        style: TextStyle(
-                                                            fontFamily: 'icons',
-                                                            color: iconColor,
-                                                            fontSize: 18),
-                                                      )),
+                                                        myCubit
+                                                            .featureList[index]
+                                                            .description!,
+                                                      ),
+                                                    ),
+                                                  )
                                                 ],
                                               ),
-                                              Padding(
-                                                padding:
-                                                    const EdgeInsets.fromLTRB(
-                                                        0, 5, 31, 0),
-                                                child: AnimatedDefaultTextStyle(
-                                                  textAlign: TextAlign.right,
-                                                  style: TextStyle(
-                                                      color: Colors.black,
-                                                      fontSize: myCubit
-                                                          .featureList[index]
-                                                          .fontSize),
-                                                  duration: const Duration(
-                                                      milliseconds: 0),
-                                                  child: Text(
-                                                    myCubit.featureList[index]
-                                                        .description!,
-                                                  ),
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                    separatorBuilder: (context, index) {
-                                      return const SizedBox(
-                                        height: 10,
-                                      );
-                                    },
-                                    itemCount: myCubit.featureList.length),
-                              )),
-                          rowWidget: const MainIconsRowWidget(
-                            icon1: 'm',
-                          ),
-                          function: () {
-                            if (isEdit == false) {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => PumpSettingsScreen(
-                                      isEdit: false,
-                                    ),
-                                  ));
-                            } else {
-                              Navigator.pop(context);
-                            }
-                          },
+                                            ),
+                                          );
+                                  },
+                                  separatorBuilder: (context, index) {
+                                    return myCubit.featuresIntList[index] == 1
+                                        ? const SizedBox(
+                                            width: 0,
+                                            height: 0,
+                                          )
+                                        : const SizedBox(
+                                            height: 10,
+                                          );
+                                  },
+                                  itemCount: myCubit.featureList.length),
+                            )),
+                        rowWidget: const MainIconsRowWidget(
+                          icon1: 'm',
                         ),
-                        const Spacer()
-                      ],
-                    ),
+                        function: () {
+                          if (isEdit == false) {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => PumpSettingsScreen(
+                                    isEdit: false,
+                                  ),
+                                ));
+                          } else {
+                            Navigator.pop(context);
+                          }
+                        },
+                      ),
+                      const Spacer()
+                    ],
                   );
           },
         ),
